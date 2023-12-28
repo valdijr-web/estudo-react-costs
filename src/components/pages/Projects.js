3d454f3905c127130d2a7146ceb8afc120885d73
valdijunior <valdi.ads@gmail.com>
2023-12-28 16:29:34 -0300

    Criando dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,9 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from "../layout/LinkButton";
+
+import styles from './Projects.module.css';
 
 function Projects() {
 
@@ -9,9 +13,15 @@ function Projects() {
         message = location.state.message;
     }
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newProject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     );
 }
